6df17860cc4f4730f6831eec8d9f0e4c29306fd2
Cody Partington <codythatsme@gmail.com>
2023-03-04 00:06:44 +1100

    image attach function added, compressor dep added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,59 @@
 import { type FormWithFields } from "~/types/prismaRelations";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { BsCameraFill } from "react-icons/bs";
+import Compressor from "compressorjs";
+import { useState } from "react";
 
 type Props = {
-  form: FormWithFields
+  form: FormWithFields;
 };
 
 const Form: React.FC<Props> = ({ form }) => {
-  return <div>Form</div>;
+  const { register, handleSubmit } = useForm();
+  const [images, setImages] = useState<string[]>([]);
+
+  const handleImageAttach = (e: any) => {
+    Array.from(e.target.files).forEach((file: any) => {
+      const reader = new FileReader();
+      reader.onload = (onLoadEvent: any) => {
+        setImages((imageState: string[]) => [
+          ...imageState,
+          onLoadEvent.target.result,
+        ]);
+      };
+      new Compressor(file, {
+        quality: 0.6,
+        maxHeight: 1422,
+        maxWidth: 800,
+        success(result) {
+          reader.readAsDataURL(result);
+        },
+      });
+    });
+  };
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <BsCameraFill className="text-3xl text-white">
+          <input
+            hidden
+            type={"file"}
+            multiple
+            {...register("file", { required: true })}
+            onChange={handleImageAttach}
+          />
+        </BsCameraFill>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Form;
--- src/pages/forms/index.tsx
+++ src/pages/forms/index.tsx
@@ -1,28 +1,15 @@
-import { useTsController } from "@ts-react/form";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
-import { z } from "zod";
+
 import Form from "~/components/Form";
 import { api } from "~/utils/api";
-
-const TextField = ({ inputType }: { inputType: string }) => {
-  const { field, error } = useTsController<string>();
-  return (
-    <>
-      <input
-        type={inputType || "text"}
-        value={field.value}
-        onChange={(e) => field.onChange(e.target.value)}
-      />
-      {error?.errorMessage && <span>{error.errorMessage}</span>}
-    </>
-  );
-};
 
 const FormContainer: NextPage = () => {
   const router = useRouter();
   const { formId } = router.query;
-  const form = api.form.getForm.useQuery({ formId: formId as string });
+  const form = api.form.getForm.useQuery({ formId: formId as string }, {
+    enabled: !!formId,
+  });
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       {/* <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 "></div> */}
