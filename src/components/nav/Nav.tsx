49cd0a01269d5e5add44f2d04e9b18f237a87346
Cody Partington <codythatsme@gmail.com>
2023-03-04 20:21:07 +1100

    nav user menu developed

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,5 +1,12 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import { FaSignOutAlt, FaUserAlt } from "react-icons/fa";
+import {
+  FaUserAlt,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaWpforms,
+} from "react-icons/fa";
+import { Menu } from "@headlessui/react";
+import Link from "next/link";
 
 const Nav = () => {
   const { data: sessionData } = useSession();
@@ -8,12 +15,51 @@ const Nav = () => {
     <div className="flex h-10 w-full items-center justify-between bg-black p-3">
       <div></div>
       {sessionData ? (
-        <FaSignOutAlt
-          onClick={() => void signOut()}
-          className="cursor-pointer text-white"
-        />
+        // <FaSignOutAlt
+        //   onClick={() => void signOut()}
+        //   className="cursor-pointer text-white"
+        // />
+        <div className="relative">
+          <Menu>
+            <Menu.Button className="p-1">
+              <FaUserAlt className="text-white" />
+            </Menu.Button>
+            <Menu.Items
+              className={"absolute top-8 right-[-10px] flex flex-col"}
+            >
+              <div className="w-32 rounded-md bg-gray-600 py-1 text-white">
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link
+                      className={` flex w-full items-center p-2 rounded-lg duration-150 ${
+                        active ? "bg-blue-500" : "bg-gray-600"
+                      }`}
+                      href="/myforms"
+                    >
+                      <FaWpforms className="text-white mr-2" />
+                      My Forms
+                    </Link>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      className={` flex w-full cursor-pointer items-center p-2 rounded-lg duration-150 ${
+                        active ? "bg-blue-500" : "bg-gray-600"
+                      }`}
+                      onClick={() => void signOut()}
+                    >
+                      <FaSignOutAlt className="mr-2"/>
+                      Sign Out
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Menu>
+        </div>
       ) : (
-        <FaUserAlt
+        <FaSignInAlt
           onClick={() => void signIn("discord")}
           className="cursor-pointer text-white"
         />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#15162c] to-[#04050a]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 "></div>
       </main>
     </>
