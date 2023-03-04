27a5817e6d886610bad06a2907cf400c9e3f0c25
Cody Partington <codythatsme@gmail.com>
2023-03-04 15:49:15 +1100

    schema updated for submission

--- src/server/api/routers/submission.ts
+++ src/server/api/routers/submission.ts
@@ -1,14 +1,45 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import cloudinary from "../../../utils/cloudinary.mjs"
+import cloudinary from "../../../utils/cloudinary.mjs";
 
 export const submissionRouter = createTRPCRouter({
-    createSubmission: publicProcedure
-        .input(
-            z.object({
-                formId: z.string(),
-                email: z.string(),
-                images: z.array(z.string()),
-                fields: z.array(
-
-})
+  createSubmission: publicProcedure
+    .input(
+      z.object({
+        formId: z.string(),
+        email: z.string(),
+        images: z.array(z.string()),
+        fields: z.array(
+          z.object({
+            id: z.string(),
+            value: z.string(),
+          })
+        ),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { formId, email, images, fields } = input;
+      const submission = await ctx.prisma.submission.create({
+        data: {
+          formId,
+          email,
+          fields,
+        },
+      });
+      const imagePromises = images.map(async (image) => {
+        return await cloudinary.uploader.upload(image, {
+          folder: "submissions",
+        });
+      });
+      const uploadedImages = await Promise.all(imagePromises);
+      ctx.prisma.image.createMany({
+        data: uploadedImages.map((image) => {
+          return {
+            submissionId: submission.id,
+            url: image.url,
+          };
+        }),
+      });
+      return submission;
+    }),
+});
