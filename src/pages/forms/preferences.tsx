2972e4105914bda3d5f3bed358b625d4cdd09eb5
Cody Partington <codythatsme@gmail.com>
2023-03-13 13:03:44 +1100

    button added to go to fields

--- src/pages/forms/preferences.tsx
+++ src/pages/forms/preferences.tsx
@@ -182,10 +182,15 @@ const FormPreferences: React.FC<FormPreferencesProps> = ({ form }) => {
           </button>
         </div>
       ) : null}
-      <input
-        className="mr-2 mb-2 cursor-pointer rounded-lg border border-red-700 px-5 py-2.5 text-center text-sm font-medium text-red-700 duration-200 hover:bg-red-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900"
-        type="submit"
-      />
+      <div>
+        <input
+          className="mr-2 mb-2 cursor-pointer rounded-lg border border-red-700 px-5 py-2.5 text-center text-sm font-medium text-red-700 duration-200 hover:bg-red-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900"
+          type="submit"
+        />
+        <button className="mr-2 mb-2 cursor-pointer rounded-lg border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800">
+          Fields
+        </button>
+      </div>
     </form>
   );
 };
